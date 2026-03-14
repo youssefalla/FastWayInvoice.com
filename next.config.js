59e5dd5ad2c8b0f83d9eca69398c32f3b1df9dc6
youssefalla <138485256+youssefalla@users.noreply.github.com>
2026-03-14 21:22:06 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/ads.txt',
+        destination: 'https://srv.adstxtmanager.com/19390/fastwayinvoice.com',
+        permanent: true,
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
