8f8fe85d280292e2dbaa5df2656f4caf3c941eb5
youssefalla <138485256+youssefalla@users.noreply.github.com>
2026-03-14 07:51:29 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -62,6 +62,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-1D1KBYHKF3"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-1D1KBYHKF3');
+            `,
+          }}
+        />
+      </head>
       <body className={`${inter.variable} ${poppins.variable} font-sans antialiased`}>
         {children}
       </body>
